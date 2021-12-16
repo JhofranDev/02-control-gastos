4b66d86caafebb1a92e526b0726d8a21905654ac
Jhofran Briceño <jhofran.dev@outlook.com>
2021-12-16 10:08:33 -0500

    0.1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
+
 import Header from "./components/Header"
-import IconoNuevoGasto from './img/nuevo-gasto.svg'
 import Modal from "./components/Modal"
+import ListadoGastos from './components/ListadoGastos'
+
+import { generarId } from './helpers'
+
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 
 
@@ -9,9 +14,26 @@ function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+  const [gastos, setGastos] = useState([])
 
   const handleNuevoGasto = () => {
     setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500)
+  }
+
+  const guardarGasto = gasto => {
+    gasto.id = generarId()
+    gasto.fecha = Date.now()
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false)
+    setTimeout(() => {
+      setModal(false)
+    }, 500)
   }
 
   return (
@@ -24,16 +46,28 @@ function App() {
       />
 
       {isValidPresupuesto && (
-        <div className='nuevo-gasto'>
-          <img 
-            src={IconoNuevoGasto}
-            alt='icono nuevo gasto'
-            onClick={handleNuevoGasto}
-          />
-        </div>
+        <>
+          <main>
+            <ListadoGastos 
+              gastos={gastos}
+            />
+          </main>
+          <div className='nuevo-gasto'>
+            <img 
+              src={IconoNuevoGasto}
+              alt='icono nuevo gasto'
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
       )}
 
-      {modal && <Modal setModal={setModal}/>}
+      {modal && <Modal 
+                  setModal={setModal} 
+                  animarModal={animarModal} 
+                  setAnimarModal={setAnimarModal}
+                  guardarGasto={guardarGasto}
+                />}
 
     </div>
   )

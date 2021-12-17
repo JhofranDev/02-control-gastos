b49e44d323e62530885a49007dbc23288d190bb1
Jhofran Briceño <jhofran.dev@outlook.com>
2021-12-17 09:51:16 -0500

    0.1.4

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
-const ControlPresupuesto = ({presupuesto}) => {
+const ControlPresupuesto = ({gastos, presupuesto}) => {
+
+  const [disponible, setDisponible] = useState(0)
+  const [gastado, setGastado] = useState(0)
+  const [porcentaje, setPorcentaje] = useState(0)
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce((total, gasto) => {
+      return gasto.cantidad + total
+    }, 0)
+
+    const totalDisponible = presupuesto - totalGastado
+
+    const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto)*100).toFixed(2)
+
+    setGastado(totalGastado)
+    setDisponible(totalDisponible)
+    
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje)
+    }, 1500)
+  }, [gastos])
+
 
   const formatearCantidad = (cantidad) => {
     return cantidad.toLocaleString('en-US', {
@@ -12,7 +36,15 @@ const ControlPresupuesto = ({presupuesto}) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <p>Grafica aquí</p>
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: '#3B82F6',
+            trailColor: '#F5F5F5',
+            textColor: '#3B82F6'
+          })}
+          value={porcentaje}
+          text={`${porcentaje}%`}
+        />
       </div>
 
       <div className="contenido-presupuesto">
@@ -20,10 +52,10 @@ const ControlPresupuesto = ({presupuesto}) => {
           <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
         </p>
         <p>
-          <span>Disponible: </span>{formatearCantidad(0)}
+          <span>Disponible: </span>{formatearCantidad(disponible)}
         </p>
         <p>
-          <span>Gastado: </span>{formatearCantidad(0)}
+          <span>Gastado: </span>{formatearCantidad(gastado)}
         </p>
       </div>
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Mensaje from './Mensaje'
 
 import Cerrar from '../img/cerrar.svg'
 
-const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
+const Modal = ({
+  setModal, 
+  animarModal, 
+  setAnimarModal, 
+  guardarGasto, 
+  gastoEditar,
+  setGastoEditar
+}) => {
   const [nombre, setNombre] = useState('')
   const [cantidad, setCantidad] = useState('')
   const [categoria, setCategoria] = useState('')
   const [mensaje, setMensaje] = useState('')
+  const [id, setId] = useState('')
+  const [fecha, setFecha] = useState('')
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0) {
+      setNombre(gastoEditar.nombre)
+      setCantidad(gastoEditar.cantidad)
+      setCategoria(gastoEditar.categoria)
+      setId(gastoEditar.id)
+      setFecha(gastoEditar.fecha)
+    }
+  }, [])
 
   const ocultarModal = () => {
     setAnimarModal(false)
-    
+    setGastoEditar({})
     setTimeout(() => {
       setModal(false)
     }, 500)
@@ -30,7 +49,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
       return
     }
 
-    guardarGasto({nombre, cantidad, categoria})
+    guardarGasto({nombre, cantidad, categoria, id, fecha})
   }
 
   return (
@@ -44,7 +63,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
       </div>
 
       <form onSubmit={handleSubmit} className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
-        <legend>Nuevo Gasto</legend>
+        <legend>{gastoEditar.nombre ? 'Editar Gasto' : 'Nuevo Gasto'}</legend>
         {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
 
         <div className='campo'>
@@ -91,7 +110,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
 
         <input 
           type='submit'
-          value='Añadir Gasto'
+          value={gastoEditar.nombre ? 'Guardar Cambios' : 'Añadir Gasto'}
         />
     
       </form>
